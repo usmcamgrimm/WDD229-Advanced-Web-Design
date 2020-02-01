a4f7388f75a380c7afa7fdf22a153042fa620f7c
Adrian Grimm <usmcamgrimm@gmail.com>
2020-02-01 15:31:54 -0500

    Tab text

---
+++ Week 3/Week3_visitOslo/js/scripts.js
@@ -0,0 +1,8 @@
+// Change the tab title
+window.onblur = function () {
+    this.document.title = 'Kom igjen!';
+}
+
+window.onfocus = function () {
+    this.document.title = 'Grimm | Velkommen!'
+}
